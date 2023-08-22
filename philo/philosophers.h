41ed5ccaa6b998a5db0e7c1d369b11b171b00dec
SalahHW <salah.bouheniche@gmail.com>
2023-08-23 01:36:23 +0200

    update structure and functions prototypes

--- philo/philosophers.h
+++ philo/philosophers.h
@@ -6,7 +6,7 @@
 /*   By: sbouheni <sbouheni@student.42mulhouse.f    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/18 16:21:36 by sbouheni          #+#    #+#             */
-/*   Updated: 2023/08/21 20:49:45 by sbouheni         ###   ########.fr       */
+/*   Updated: 2023/08/23 00:30:31 by sbouheni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,23 +19,18 @@
 # include <sys/time.h>
 # include <unistd.h>
 
-typedef enum e_state
-{
-	DEAD = 0,
-	EATING = 1,
-	SLEEPING = 2,
-	THINKING = 3,
-}					e_state;
-
 typedef struct s_philo
 {
 	int				id;
+	int				dead;
+	int				simulation_running;
+	size_t			start_time;
 	size_t			time_to_die;
 	size_t			time_to_eat;
 	size_t			time_to_sleep;
 	int				nb_eat;
+	int 			nb_eat_done;
 	size_t			last_meal;
-	e_state			state;
 	pthread_mutex_t	fork;
 	pthread_t		thread;
 	struct s_philo	*next;
@@ -44,6 +39,8 @@ typedef struct s_philo
 
 typedef struct s_data
 {
+	int				simulation_running;
+	size_t			start_time;
 	int				nb_philo;
 	size_t			time_to_die;
 	size_t			time_to_eat;
@@ -61,12 +58,20 @@ int					print_error(char *str);
 // init.c
 int					add_new_philo(t_data *data);
 t_data				*init_data(int argc, char **argv);
-void					init_philo_data(t_data *data);
+void				init_philo_data(t_data *data);
 // utils.c
 int					ft_atoi(const char *str);
+// time.c
 size_t				get_time_stamp(void);
+size_t				get_program_time(size_t start_time);
+void				my_usleep(size_t time);
 // routine.c
 void				*philo_routine(void *arg);
 void				*health_routine(void *arg);
+// state.c
+int					eating(t_philo *philo);
+int					sleeping(t_philo *philo);
+int					thinking(t_philo *philo);
+int					check_philo_health(t_philo *philo);
 
 #endif
