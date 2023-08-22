6c5dc753165aa3c67246d638ca0858a8ae844432
SalahHW <salah.bouheniche@gmail.com>
2023-08-23 01:35:08 +0200

    simplify with subfunctions calls

--- philo/main.c
+++ philo/main.c
@@ -6,43 +6,44 @@
 /*   By: sbouheni <sbouheni@student.42mulhouse.f    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/18 16:19:22 by sbouheni          #+#    #+#             */
-/*   Updated: 2023/08/21 23:18:54 by sbouheni         ###   ########.fr       */
+/*   Updated: 2023/08/22 21:39:58 by sbouheni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philosophers.h"
 
-int	main(int argc, char **argv)
+static void create_philo(t_data *data)
 {
-	t_data	*data;
-	t_philo	*philo;
-	int		i;
+	int i;
 
 	i = 0;
-	if (parse_arg(argc, argv) == -1)
-		return (-1);
-	data = init_data(argc, argv);
-	if (!data)
-		return (-1);
 	while (i < data->nb_philo)
 	{
 		add_new_philo(data);
 		i++;
 	}
-	init_philo_data(data);
-	if (!pthread_create(&data->health, NULL, health_routine, data))
+}
+
+static void launch_philo_threads(t_data *data)
+{
+	t_philo *philo;
+	int i;
+	
+	i = 0;
+	philo = data->first_philo;
+	while (i < data->nb_philo)
 	{
-		i = 0;
-		philo = data->first_philo;
-		while (i < data->nb_philo)
-		{
-			if (!pthread_create(&philo->thread, NULL, philo_routine, philo))
-			{
-				philo = philo->next;
-				i++;
-			}
-		}
+		pthread_create(&philo->thread, NULL, philo_routine, philo);
+		philo = philo->next;
+		i++;
 	}
+}
+
+static void join_philo_threads(t_data *data)
+{
+	t_philo *philo;
+	int i;
+	
 	i = 0;
 	philo = data->first_philo;
 	while (i < data->nb_philo)
@@ -51,5 +52,21 @@ int	main(int argc, char **argv)
 		philo = philo->next;
 		i++;
 	}
+}
+
+int	main(int argc, char **argv)
+{
+	t_data	*data;
+
+	if (parse_arg(argc, argv) == -1)
+		return (-1);
+	data = init_data(argc, argv);
+	if (!data)
+		return (-1);
+	create_philo(data);
+	init_philo_data(data);
+	pthread_create(&data->health, NULL, health_routine, data);
+	launch_philo_threads(data);
+	join_philo_threads(data);
 	pthread_join(data->health, NULL);
 }
