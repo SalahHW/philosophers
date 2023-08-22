991ee5eb70de5ef2f65afced6f55d9122f6ab019
SalahHW <salah.bouheniche@gmail.com>
2023-08-23 01:36:51 +0200

    update simulation

--- philo/routine.c
+++ philo/routine.c
@@ -6,47 +6,64 @@
 /*   By: sbouheni <sbouheni@student.42mulhouse.f    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/20 14:19:22 by sbouheni          #+#    #+#             */
-/*   Updated: 2023/08/21 23:03:45 by sbouheni         ###   ########.fr       */
+/*   Updated: 2023/08/23 01:30:55 by sbouheni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philosophers.h"
 
-void	*philo_routine(void *arg)
+static	void stop_philo_simulation(t_data *data)
 {
 	t_philo *philo;
+	int i;
+
+	i = 0;
+	philo = data->first_philo;
+	while (i < data->nb_philo)
+	{
+		philo->simulation_running = 0;
+		philo = philo->next;
+		i++;
+	}
+}
+
+static int all_philo_stopped(t_data *data)
+{
+	t_philo *philo;
+	int i;
+
+	philo = data->first_philo;
+	i = 0;
+	while (i < data->nb_philo)
+	{
+		if (philo->simulation_running)
+			return (0);
+		philo = philo->next;
+		i++;
+	}
+	return (1);
+}
+
+void	*philo_routine(void *arg)
+{
+	t_philo	*philo;
 
 	philo = (t_philo *)arg;
 	philo->last_meal = get_time_stamp();
-	while (philo->state != DEAD)
+	if (philo->id % 2 == 0)
+		usleep(500);
+	while (!philo->dead && philo->simulation_running && (philo->nb_eat == -1
+		|| philo->nb_eat_done < philo->nb_eat))
 	{
-		if (philo->state == EATING)
-		{
-			printf("%lu %d is eating\n", get_time_stamp(), philo->id);
-			usleep(philo->time_to_eat * 1000);
-			philo->last_meal = get_time_stamp();
-			pthread_mutex_unlock(&philo->fork);
-			pthread_mutex_unlock(&philo->next->fork);
-			philo->state = SLEEPING;
-		}
-			
-		else if (philo->state == SLEEPING)
-		{
-			printf("%lu %d is sleeping\n", get_time_stamp(), philo->id);
-			usleep(philo->time_to_sleep * 1000);
-			philo->state = THINKING;
-
-		}
-		else if (philo->state == THINKING)
-		{
-			printf("%lu %d is thinking\n", get_time_stamp(), philo->id);
-			pthread_mutex_lock(&philo->fork);
-			pthread_mutex_lock(&philo->next->fork);
-			printf("%lu %d has taken a fork\n", get_time_stamp(), philo->id);
-			philo->state = EATING;
-		}
+		if (!eating(philo))
+			return (NULL);
+		if (philo->nb_eat_done == philo->nb_eat && philo->nb_eat != -1)
+			return (NULL);
+		if (!sleeping(philo))
+			return (NULL);
+		if (!thinking(philo))
+			return (NULL);
 	}
-	printf("%lu %d is dead\n", get_time_stamp(), philo->id);
 	return (NULL);
 }
 
@@ -54,17 +71,20 @@ void	*health_routine(void *arg)
 {
 	t_data *data;
 	t_philo *philo;
-
 	data = (t_data *)arg;
 	philo = data->first_philo;
-	while (philo->state != DEAD)
+	while (data->simulation_running)
 	{
-		printf("checking philo %d\n", philo->id);
-		if (get_time_stamp() - philo->last_meal > philo->time_to_die)
+		// refresh_philo_data(philo);
+		if (philo->dead)
 		{
-			philo->state = DEAD;
-			printf("%lu %d is dead\n", get_time_stamp(), philo->id);
+			stop_philo_simulation(data);
 			return (NULL);
+		}
+		if (!philo->simulation_running)
+		{
+			if (all_philo_stopped(data))
+				return (NULL);
 		}
 		philo = philo->next;
 	}
