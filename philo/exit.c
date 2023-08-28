5d5c3c208dc82107942e2c8df72bffcd15942568
SalahHW <salah.bouheniche@gmail.com>
2023-08-28 22:44:03 +0200

    rename error.c to exit.c

--- philo/exit.c
+++ philo/exit.c
@@ -1,19 +1,41 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   error.c                                            :+:      :+:    :+:   */
+/*   exit.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: sbouheni <sbouheni@student.42mulhouse.fr>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/18 17:20:56 by sbouheni          #+#    #+#             */
-/*   Updated: 2023/08/24 22:35:13 by sbouheni         ###   ########.fr       */
+/*   Updated: 2023/08/28 22:16:47 by sbouheni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philosophers.h"
+#include <pthread.h>
 
-int	print_error(char *str)
+static void	destroy_mutex(t_philo *philo)
 {
-	printf("%s", str);
-	return (-1);
+	pthread_mutex_destroy(&philo->fork);
+	pthread_mutex_destroy(&philo->nb_eat_done_mutex);
+	pthread_mutex_destroy(&philo->last_meal_mutex);
+}
+
+void	clear_all(t_philo *philo)
+{
+	t_philo	*tmp_philo;
+	int		nb_philo;
+	int		i;
+
+	i = 0;
+	nb_philo = philo->data->nb_philo;
+	while (i < nb_philo)
+	{
+		destroy_mutex(philo);
+		tmp_philo = philo->next;
+		if (i == nb_philo - 1)
+			pthread_mutex_destroy(&philo->data->sim_running_mutex);
+		free(philo);
+		philo = tmp_philo;
+		i++;
+	}
 }
